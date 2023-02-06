66573a5e4ac64e475a1bf9caf0bdd22271c38b6e
skay138 <nine1352@gmail.com>
2023-02-06 11:22:55 +0900

    .

--- account/serializer.py
+++ account/serializer.py
@@ -1,11 +1,36 @@
 from rest_framework import serializers
 from account.models import User, Follow
+from post.models import Post
+
 
 class UserSerializer(serializers.ModelSerializer):
 
+    def getTotalPost(self, obj):
+        uid = obj.uid
+        totalPost = Post.objects.filter(author_id = uid).count()
+        return totalPost
+
+    def getFollower(self, obj):
+        uid = obj.uid
+        follwer = Follow.objects.filter(followee = uid).count()
+        return follwer
+
+    def getFollowee(self, obj):
+        uid = obj.uid
+        followee = Follow.objects.filter(follower = uid).count()
+        return followee
+
+
+    
+    totalPost = serializers.SerializerMethodField('getTotalPost')
+    follower = serializers.SerializerMethodField('getFollower')
+    followee = serializers.SerializerMethodField('getFollowee')
+
+
+
     class Meta :
         model = User
-        fields = ['uid','email','nickname','introduce','image', 'bgimage' ,'gender', 'birth', 'is_signed','is_active']
+        fields = ['uid','email','nickname','introduce','image', 'bgimage', 'totalPost', 'follower', 'followee', 'gender', 'birth', 'is_signed','is_active']
 
 class SwaggerDeleteSerializer(serializers.ModelSerializer):
 
